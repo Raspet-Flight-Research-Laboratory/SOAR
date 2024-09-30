6a5a32db6ad54873af850e143de2f404f6bf52ac
Nathaniel <nac294@msstate.edu>
2024-09-30 09:21:48 -0500

    Added an actor for data manipulations.

---
+++ Unreal/Environments/Blocks/Source/Blocks/DataManipulatior.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataManipulatior.h"
+
+// Sets default values
+ADataManipulatior::ADataManipulatior()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADataManipulatior::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADataManipulatior::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
